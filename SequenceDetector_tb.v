94b9d2b00aba3d60e2672bc1aa4d7970a04db4f0
Nitish Diwakar <74351773+ntsdwkr@users.noreply.github.com>
2021-02-06 00:45:51 +0530

    Update SequenceDetector_tb.v

--- SequenceDetector_tb.v
+++ SequenceDetector_tb.v
@@ -1,5 +1,4 @@
-`timescale 1s/100ms
-`include "SeqDet.v"
+
 module SeqDet_tb;
     reg in_bit,clk,reset;
     wire out;
@@ -8,8 +7,6 @@ module SeqDet_tb;
 initial
 begin
     $monitor("time = %0t || input = %0b || output = %0b", $time, in_bit, out);
-    $dumpfile("SeqDet.vcd");
-    $dumpvars;
         clk = 1'b0;
         reset = 1'b1;
         #15 reset = 1'b0; 
